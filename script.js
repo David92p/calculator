a7d163c1eb16e89c29c9be245f3de7274cc41a1c
David92p <dave.panetta92@gmail.com>
2022-06-09 16:22:50 +0200

    add script and create page container

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const body = document.getElementsByTagName('body');
+const container = document.createElement('div');
+document.body.appendChild(container)
+
+container.classList.add('container')
+container.setAttribute('id', 'color-container')
+
