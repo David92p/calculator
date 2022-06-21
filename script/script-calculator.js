70e5d61f7b3097f6fec5a307d38c82b1d2b5b0bf
David92p <dave.panetta92@gmail.com>
2022-06-21 18:50:52 +0200

    add functions to operator management

--- script/script-calculator.js
+++ script/script-calculator.js
@@ -1,48 +1,57 @@
 
 createCalculator(calculator);
 
+function clear(firstOperand, secondOperand){
+    let text = ''
+    firstOperand.innerText = text
+    secondOperand.innerText = text
+};
+
 const firstOperand = document.querySelector('.first-operand');
 const secondOperand = document.querySelector('.second-operand');
-
-
+const result = document.querySelector('.result')
 
 const numbers = document.querySelectorAll('#date-number');
-const operators = document.querySelectorAll('#operator')
+const operators = document.querySelectorAll('#operator');
 
-const allClear = document.getElementById('all-clear');
-const clear = document.getElementById('delete');
+const allDelete = document.getElementById('all-delete');
+const del = document.getElementById('delete')
 
-let writeNumbers = (numbersCalculator, operand) => {
-    operand.innerHTML = "0"
-    numbersCalculator.forEach((el,i) => {
-        let count = ""
-        if (el.addEventListener('click', () => {
 
-        })){
-            console.log(count);
-        }
-        else if (i == 9){
-            el.addEventListener('click', () => {
-                let n = "."
-                count += n
-                firstOperand.innerHTML = count
-            })
-        }
-        else if (i == 10){
-            el.addEventListener('click', () => {
-                let n = "0"
-                count += n
-                firstOperand.innerHTML = count
-            })
-        }
-        else el.addEventListener('click', () => {
-            let n = i + 1
-            count += n.toString()
-            firstOperand.innerHTML = count
-        })
-        
-     })
+clear(firstOperand, secondOperand)
+
+function valueClick(numbersCalculator){
+    numbersCalculator.forEach(el => {
+        el.addEventListener('click', () => el.innerText)
+    })
 }
-    
-writeNumbers(numbers, firstOperand);
+
+function updateDisplay(operand, numbersCalculator){
+    let n = ''
+    //n += valueClick(numbersCalculator)
+    if (n[0] == '.' || n[0] == '0') return 
+    else if (n.length > 29) return 
+    else operand.innerText = n
+}
+
+updateDisplay(firstOperand, numbers)
+
+// updateDisplay(firstOperand, numbers)
+
+// function valueClick (numbersCalculator){
+//     numbersCalculator.forEach(el => {
+//         el.addEventListener('click', () => {
+//             let n = el.innerText
+//             return n
+//         })  
+//     })  
+// };
+
+
+// secondOperand.innerText = '123456789'
+// result.innerText = '123456789'
+// console.log(firstOperand);
+// console.log(secondOperand);
+// console.log(result);
+// console.log(x.length);
 
