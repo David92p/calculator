8fe62fb86882d77bcb2996bbbb35ec0c576dd9ff
David92p <dave.panetta92@gmail.com>
2022-06-23 15:55:15 +0200

    fix function

--- script/script-calculator.js
+++ script/script-calculator.js
@@ -6,37 +6,101 @@ const secondOperand = document.querySelector('.second-operand');
 const result = document.querySelector('.result')
 
 const numbers = document.querySelectorAll('#date-number');
-const operators = document.querySelectorAll('#operator');
+//const operators = document.querySelectorAll('#operator');
 
 const allDelete = document.getElementById('all-delete');
 const del = document.getElementById('delete')
 
-function valueClick(btn, operand){
-    if (operand.innerHTML[0] === '0' || operand.innerHTML[0] === '.'){
-        operand.innerHTML = ''
-    }
-    else{
-        operand.innerHTML += btn.innerText
-    }   
+function startCalc(operand, numbers){
+    numbers.forEach((el) => {
+        el.addEventListener('click', function(){
+            let last = operand.innerHTML.length -1
+            if (
+                operand.innerHTML[0] === '0' || 
+                operand.innerHTML[0] === '.' ||
+                operand.innerHTML[0] === ' + ' || 
+                operand.innerHTML[0] === ' - ' ||
+                operand.innerHTML[0] === ' * ' ||
+                operand.innerHTML[0] === ' ÷ ' 
+                )
+            {
+                operand.innerHTML = ''
+            } 
+            else if (el.innerText === 'DEL'){
+                let x = operand.innerHTML.slice(0, last)
+                operand.innerHTML = x
+            }
+            else if (operand.innerHTML[last] === ''){
+                //Enter the new function 
+                return
+            }
+            else if (
+                el.innerText === ' + ' ||
+                el.innerText === ' - ' ||
+                el.innerText === ' * ' ||
+                el.innerText === ' ÷ '
+            ){
+                operand.innerHTML += el.innerText 
+            }
+            else if (operand.innerText.length < 25){
+                operand.innerHTML += el.innerText
+            }
+        })}
+    )
 }
 
-function updateDisplay(operand, numbersCalc){
-    for(let btn of numbersCalc){
-        btn.addEventListener('click', function(){    
-            valueClick(btn, operand)
+
+function enterOperators(operand, operator){
+    operator.forEach((el) => {
+        el.addEventListener('click', function(){
+            if (operand.innerHTML[0] === '+' || 
+                operand.innerHTML[0] === '.' ||
+                operand.innerHTML[0] === '*' ||
+                operand.innerHTML[0] === '÷' 
+            ){
+                operand.innerHTML = ''
+            } 
+            else {
+                operand.innerHTML += ' ' + el.innerText + " "
+            }
         })
-    }
+    })
 }
 
-function defineValues(){
-    if (firstOperand.innerHTML.length == 0){
-        updateDisplay(firstOperand, numbers)
-    }
-}
+startCalc(firstOperand, numbers)
+
+//console.log(numbers);
+//enterOperators(firstOperand, operators)
 
 
+// function valueClickNum(btn, operand){
+//     if (operand.innerHTML[0] === '0' || operand.innerHTML[0] === '.'){
+//         operand.innerHTML = ''
+//     }
+//     else{
+//         operand.innerHTML += btn.innerText
+//     }   
+// }
 
-updateDisplay(firstOperand, numbers)
-updateDisplay(secondOperand, numbers)
+
+// function updateDisplay(operand, numbersCalc){
+//     for(let btn of numbersCalc){
+//         btn.addEventListener('click', function(){ 
+//             if (operand.innerHTML.length < 25){
+//                 valueClickNum(btn, operand) 
+//             } 
+//         })
+//     }
+// }
+
+// function defineValues(operand, numbersCalc){
+//     if (operand.innerHTML.length == 0){
+//         updateDisplay(operand, numbersCalc)
+//     }
+// }
+
+// defineValues(firstOperand, numbers)
+
+
 
 
